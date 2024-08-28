7a08920d65482e605b2287c78069847c1d5c7eb6
netoptc <joaoqdev@gmail.com>
2024-08-27 21:24:11 -0300

    add normalized function

--- src/clinets/__test__/stormGlass.test.ts
+++ src/clinets/__test__/stormGlass.test.ts
@@ -1,16 +1,18 @@
-import { StormGlass } from "@src/clinets/StormGlass"
-import axios from "axios"
+import { StormGlass } from '@src/clinets/StormGlass';
+import axios from 'axios';
+import stormglassNormalizedResponse3Hours from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+import stormglassWeather3Hours from '@test/fixtures/stormglass_weather_3_hours.json';
 
 jest.mock('axios');
 
-describe("StormGlass client", () => {
-    it('should return the normalized forecast from the StormGlass serive', async () => {
-        const lat = -33.7912
-        const lng = 123.124882
-        
-        axios.get = jest.fn().mockResolvedValue({})
-        const stormGlass = new StormGlass(axios);
-        const response = await stormGlass.fetchPoints(lat, lng);
-        expect(response).toEqual({});
-    })
-})
+describe('StormGlass client', () => {
+  it('should return the normalized forecast from the StormGlass serive', async () => {
+    const lat = -33.7912;
+    const lng = 123.124882;
+
+    axios.get = jest.fn().mockResolvedValue({ data: stormglassWeather3Hours });
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stormglassNormalizedResponse3Hours);
+  });
+});
