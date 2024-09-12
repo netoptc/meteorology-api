c75650f5bc97ae0ed1b56124d53f6eaaef8aa1c6
netoptc <joaoqdev@gmail.com>
2024-09-11 21:43:04 -0300

    add beaches functional tests

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,5 +1,7 @@
-describe('Beaches functional tests', () => {
+import { Beach } from "@src/models/beache";
 
+describe('Beaches functional tests', () => {
+    beforeAll(async () => await Beach.deleteMany({}));
     describe('When creating a new beach', () => {
         it('should create a beach with success', async () => {
             const newBeach = {
@@ -15,4 +17,24 @@ describe('Beaches functional tests', () => {
             expect(response.body).toEqual(expect.objectContaining(newBeach));
         })
     })
+
+    it('should return 422 when there is a validation error', async () => {
+        const newBeach = {
+          lat: 'invalid_string',
+          lng: 151.289824,
+          name: 'Manly',
+          position: 'E',
+        };
+        const response = await global.testRequest.post('/beaches').send(newBeach);
+  
+        expect(response.status).toBe(422);
+        expect(response.body).toEqual({
+          error:
+            'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+        });
+      });
+  
+      it.skip('should return 500 when there is any error other than validation error', async () => {
+        //TODO think in a way to throw a 500
+      });
 })
